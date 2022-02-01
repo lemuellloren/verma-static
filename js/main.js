f05a0f2d1f12f745f2a68fed038c9310e37def70
lemuellloren <lempakz@yahoo.com>
2022-02-01 13:31:03 +0800

    add lotties files and set up benefits section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+$(document).ready(function () {
+    $(document).ready(function () {
+        lottie.loadAnimation({
+            container: document.getElementById('hero-banner-wave'), // the dom element that will contain the
+            animation,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: './assets/waves/data-color-1.json' // the path to the animation json
+        });
+    });
+
+});
